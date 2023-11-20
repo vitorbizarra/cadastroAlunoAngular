7d6460a12412a737439e3cf1ee40074bd670c80a
vitorbizarra <vitorbizarracontato@gmail.com>
2023-11-20 16:23:32 -0300

    :construction: StudentsComponent

---
+++ src/app/students/students.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-students',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './students.component.html',
+  styleUrl: './students.component.css'
+})
+export class StudentsComponent {
+
+}
